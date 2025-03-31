8b2fb293e0d0003c5fb0c8cf967422a1d706ce48
Andrea Zummerova <adkabodiova@gmail.com>
2025-03-31 23:02:57 +0200

    filter design

--- src/components/FilterOfExercises.tsx
+++ src/components/FilterOfExercises.tsx
@@ -21,71 +21,78 @@ function FilterOfExercises() {
     return (
         <div className="full-size-container"> 
             <div className="filter-container">
-                <div className="filter">
-                    <form>
-                        <input type='text' name='text'/>
-                        <input type="submit" value='Hľadať'/>
+                <div className="menu">
+                <div className="overlay"></div>
+                    <form className="search">
+                        <input type='text' name='text' placeholder="Vyhľadať cvik..."/>
+                        <input type="submit" className='login' value='Hľadať'/>
                     </form>
-                    <form>
-                        <h3>Filtrovať</h3>
-                        <div className="filterInputContainer">
-                            <h4>Cieľová partia</h4>
-                            <div className="filterInput">
-                                <input id='ruky' type="checkbox"/>
-                                <label htmlFor="ruky">Ruky</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='nohy' type="checkbox"/>
-                                <label htmlFor="nohy">Nohy</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='brucho' type="checkbox"/>
-                                <label htmlFor="brucho">Brucho</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='zadok' type="checkbox"/>
-                                <label htmlFor="zadok">Zadok</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='kondička' type="checkbox"/>
-                                <label htmlFor="kondička">Kondička</label>
-                            </div>
-                        </div>
-                        <div className="filterInputContainer">
-                            <h4>Obtiažnosť</h4>
-                            <div className="filterInput">
-                                <input id='ľahké' type="checkbox"/>
-                                <label htmlFor="ľahké">Ľahké</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='stredné' type="checkbox"/>
-                                <label htmlFor="stredné">Stredné</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='ťažké' type="checkbox"/>
-                                <label htmlFor="ťažké">Ťažké</label>
+                    <form className="filter">
+                        <h2>Filter</h2>
+                        <div className="selection">
+                            <h3>Cieľová partia</h3>
+                            <div className="selectionButtons">
+                                <div className="inputLabel">
+                                    <input id='ruky' type="checkbox"/>
+                                    <label htmlFor="ruky" className="button">Ruky</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='nohy' type="checkbox"/>
+                                    <label htmlFor="nohy" className="button">Nohy</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='brucho' type="checkbox"/>
+                                    <label htmlFor="brucho" className="button">Brucho</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='zadok' type="checkbox"/>
+                                    <label htmlFor="zadok" className="button">Zadok</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='kondička' type="checkbox"/>
+                                    <label htmlFor="kondička" className="button">Kondička</label>
+                                </div>
                             </div>
                         </div>
-                        <div className="filterInputContainer">
-                            <h4>Pomôcky</h4>
-                            <div className="filterInput">
-                                <input id='činky' type="checkbox"/>
-                                <label htmlFor="činky">Činky</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='kettlebell' type="checkbox"/>
-                                <label htmlFor="ketlebell">Kettlebell</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='expander' type="checkbox"/>
-                                <label htmlFor="expander">Expander</label>
-                            </div>
-                            <div className="filterInput">
-                                <input id='slider' type="checkbox"/>
-                                <label htmlFor="slider">Slider</label>
+                        <div className="selection">
+                            <h3>Obtiažnosť</h3>
+                            <div className="selectionButtons">
+                                <div className="inputLabel">
+                                    <input id='ľahké' type="checkbox"/>
+                                    <label htmlFor="ľahké" className="button">Ľahké</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='stredné' type="checkbox"/>
+                                    <label htmlFor="stredné" className="button">Stredné</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='ťažké' type="checkbox"/>
+                                    <label htmlFor="ťažké" className="button">Ťažké</label>
+                                </div>
                             </div>
                         </div>
-                        
+                        <div className="selection">
+                            <h3>Pomôcky</h3>
+                            <div className="selectionButtons">
+                                <div className="inputLabel">
+                                    <input id='činky' type="checkbox"/>
+                                    <label htmlFor="činky" className="button">Činky</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='kettlebell' type="checkbox"/>
+                                    <label htmlFor="ketlebell" className="button">Kettlebell</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='expander' type="checkbox"/>
+                                    <label htmlFor="expander" className="button">Expander</label>
+                                </div>
+                                <div className="inputLabel">
+                                    <input id='slider' type="checkbox"/>
+                                    <label htmlFor="slider" className="button">Slider</label>
+                                </div>
+                            </div>
+                        </div>
+                        <button className="login">Filtrovať</button>
                     </form>
                 </div>
             </div>
